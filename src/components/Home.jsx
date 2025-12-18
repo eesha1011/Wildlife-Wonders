7a74796763d035284a88a98b782f4ada09b3088c
Pratik <tpratik083@gmail.com>
2025-12-18 16:11:25 +0530

    Home Page Add by - Pratik

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="p-6 mt-30 text-center items-center justify-center">
+      <h1 className="text-3xl font-bold text-green-700">
+        Welcome to Wildlife Wonders 🐶🐦
+      </h1>
+      <p className="mt-4 text-gray-700">
+        Report injured animals and help save lives ❤️
+      </p>
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className='bg-green-600 text-white p-4 flex gap-6'>
         <h2>🐾 Wildlife Wonders</h2>
         <Link to='/'>Home</Link>
-        <Link to='/report'>Rpoert</Link>
+        <Link to='/report'>Report</Link>
         <Link to='/admin'>Admin</Link>
     </nav>
   )
