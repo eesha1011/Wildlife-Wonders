724be9b1ffc730d19e1da237f426fa3c9346c6a2
Eesha Sherikar <sherikareesha@gmail.com>
2025-12-18 16:44:02 +0530

    navbar updated - eesha

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+
+    const baseClass = "font-semibold cursor-pointer rounded-md px-2";
+    const activeClass = "bg-green-300 hover:bg-green-200";
+    const inactiveClass = "hover:bg-neutral-200";
+
   return (
-    <nav className='bg-green-600 text-white p-4 flex gap-6'>
-        <h2>🐾 Wildlife Wonders</h2>
-        <Link to='/'>Home</Link>
-        <Link to='/report'>Rpoert</Link>
-        <Link to='/admin'>Admin</Link>
+    <nav className='bg-green-600 text-white text-lg p-4 flex justify-between gap-6'>
+        <h2 className='font-semibold'>🐾 Wildlife Wonders</h2>
+        <div className='flex justify-evenly gap-10 '>
+            <Link to='/' className={`${baseClass} ${location.pathname === "/" ? activeClass : inactiveClass}`}>Home</Link>
+            <Link to='/report' className={`${baseClass} ${location.pathname === "/report" ? activeClass : inactiveClass}`}>Report</Link>
+            <Link to='/admin'className={`${baseClass} ${location.pathname === "/admin" ? activeClass : inactiveClass}`}>Admin</Link>
+        </div> 
+        
     </nav>
   )
 }
